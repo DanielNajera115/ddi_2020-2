d9a950c6f15db93995bd074109c30cf166b257b2
DanielNajera115 <47648242+DanielNajera115@users.noreply.github.com>
2020-11-18 21:33:03 -0800

    VR_practica

---
+++ VR_Practica/Assets/Locomotion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locomotion : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector3 heightOffset;
+    public AudioSource source;
+    public AudioClip clip;
+    // Start is called before the first frame update
+    public void TeleportPlayer(Vector3 newPosition)
+    {
+        source.PlayOneShot(clip);
+        player.position = newPosition + heightOffset;
+    }
+
+}
