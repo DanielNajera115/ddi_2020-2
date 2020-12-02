32d43a580281e26cff9fa185c213bca315e8c207
DanielNajera115 <47648242+DanielNajera115@users.noreply.github.com>
2020-12-01 19:04:44 -0800

    Watson practica

---
+++ VoiceRecognition/Assets/Watson/unity-sdk-4.8.0/Examples/ScriptsForPractice/CommandRecognition.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandRecognition : MonoBehaviour
+{
+    string[] commandArray = {"change color","square disable","square enable"};
+    // Start is called before the first frame update
+    public Interactable interactable;
+    private string[] commandSplitted;
+    private bool lampLightState = false;
+   
+   public bool LampToggle(string lampState){
+        if(lampState.Equals("on")){
+            return true;
+        }
+        return false;
+   }
+
+    public void CommandRecognitionWithWatson(string finalString)
+    {
+        commandSplitted = finalString.Split(' ');
+        switch(commandSplitted[0]){
+            case "turn":
+                lampLightState = LampToggle(commandSplitted[1]);
+                interactable.TurnLight(lampLightState);
+                break;
+            case "change":
+                interactable.ChangeColor(commandSplitted[1]);
+                break;
+            case "enable":
+                interactable.SpawnObject(commandSplitted[1]);
+                break;
+            default:
+                Debug.Log("Holo");
+                break;
+        }
+    }
+}
---
+++ VoiceRecognition/Assets/Watson/unity-sdk-4.8.0/Examples/ScriptsForPractice/Interactable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public Light light;
+    public Material squareMaterial;
+    private Color[] colors= new Color[6];
+    public GameObject sword;
+    // Start is called before the first frame update
+    void Start()
+    {
+        colors[0] = Color.cyan;
+         colors[1] = Color.red;
+         colors[2] = Color.green;
+         colors[4] = Color.yellow;
+         colors[5] = Color.magenta;
+    }
+
+    public void TurnLight(bool lightState){
+        light.enabled=lightState;
+    }
+
+    public void ChangeColor(string colorCommand){
+        if(colorCommand.Equals("color"))
+            squareMaterial.color = colors[Random.Range(0,6)];
+    }
+    public void SpawnObject(string commandObject){
+        if(commandObject.Equals("weapon")){
+            sword.SetActive(true);
+        }
+    }
+}
